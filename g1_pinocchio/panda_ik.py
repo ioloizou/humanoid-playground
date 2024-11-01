73272202c1d613ceb7d817217ae718236c3a0b96
ioloizou <yiannisloizou@gmail.com>
2024-11-01 12:41:48 +0100

    added close loop inverse kinematics

--- g1_pinocchio/panda_ik.py
+++ g1_pinocchio/panda_ik.py
@@ -47,9 +47,61 @@ def update_visual_frame(viz, model, data, frame_name):
   oMf_hand = data.oMf[model.getFrameId(frame_name)]
   ee_frame.set_transform(oMf_hand.homogeneous)
 
-  meshcat_shapes.frame(ee_frame)
+  meshcat_shapes.frame(ee_frame, axis_length=0.2)
 
-def simulate_movement(viz, model, data, q_initial, v_desired, new_window, duration=1, steps=100, q_previous=None):
+def compute_ik(model, data, q_current, v_desired, frame_name, dt):
+    """
+    Computes the next configuration of the robot given the desired velocity using inverse kinematics.
+
+    Args:
+    model (pinocchio.Model): The robot model.
+    data (pinocchio.Data): The robot data.
+    q_current (np.ndarray): The current configuration of the robot.
+    v_desired (np.ndarray): The desired velocity of the end effector.
+    frame_name (str): The name of the frame to control.
+    dt (float): The time step for integration.
+
+    Returns:
+    np.ndarray: The next configuration of the robot.
+    """
+    idx_tool = model.getFrameId(frame_name)
+    J = pin.computeFrameJacobian(model, data, q_current, idx_tool, pin.LOCAL_WORLD_ALIGNED)
+    qdot = np.linalg.pinv(J) @ v_desired
+    q_next = pin.integrate(model, q_current, qdot * dt)
+    return q_next
+
+def compute_close_loop_ik(model, data, q_current, oMgoal, frame_name_id, dt):
+
+    # Get the current placement of the end effector
+    oMtool = data.oMf[frame_name_id]
+
+    # Compute the error
+    toolMgoal = oMtool.actInv(oMgoal)
+    error = pin.log6(toolMgoal).vector
+
+    # Compute the Jacobian
+    J = pin.computeFrameJacobian(model, data, q_current, idx_tool, pin.LOCAL)
+
+    # Map the Jacobian to the tangent space
+    J = pin.Jlog6(toolMgoal.inverse()) @ J
+    
+    # Compute the pseudo-inverse of the Jacobian
+    J_pinv = np.linalg.pinv(J)
+
+    gain = 1.5
+
+    # Compute the joint velocity
+    qdot = gain * J_pinv @ error
+
+    # Integrate the joint velocity
+    q_next = pin.integrate(model, q_current, qdot * dt)
+
+    # Compute the error norm
+    error_norm = np.linalg.norm(error)
+
+    return q_next, error_norm
+
+def simulate_movement(viz, model, data, q_initial, v_desired, new_window, duration=10, steps=1000, q_previous=None):
     """
     Simulates the movement of the robot for a specified duration.
 
@@ -81,23 +133,25 @@ def simulate_movement(viz, model, data, q_initial, v_desired, new_window, durati
     start_time = time.time()
     dt = duration / steps
 
-    while time.time() - start_time < duration:
+    while (time.time() - start_time < duration):
         pin.forwardKinematics(model, data, q_current)
         pin.updateFramePlacements(model, data)
 
-        # Frame name of the end effector
-        frame_name = "panda_hand"
-        idx_tool = model.getFrameId(frame_name)
+        # Uncomment the line below to use the simple IK method and comment the next line
+        # q_current = compute_ik(model, data, q_current, v_desired, frame_name, dt)
 
-        J = pin.computeFrameJacobian(model, data, q_current, idx_tool, pin.LOCAL_WORLD_ALIGNED) 
+        q_current, error_norm = compute_close_loop_ik(model, data, q_current, oMgoal, idx_tool, dt)
+        
+        print(f"Error norm: {error_norm}")
 
-        qdot = np.linalg.pinv(J) @ v_desired
-
-        q_current = pin.integrate(model, q_current, qdot * dt)
+        # Break if the error is small
+        if error_norm < 1e-2:
+            break
 
         update_visual_frame(viz, model, data, frame_name)
         display_robot_configuration(q_current, viz)
 
+        print(f"Time passed: {time.time() - start_time}")
         time.sleep(dt)  # Sleep to simulate real-time update
     
     return q_current
@@ -122,6 +176,22 @@ display_robot_configuration(q0, viz)
 v_desired = np.array([0.5, 0., 0., 0., 0., 0.])
 
 q_current = q0.copy()
+
+# Update q and frames
+pin.forwardKinematics(model, data, q_current)
+pin.updateFramePlacements(model, data)
+
+# Frame name of the end effector
+frame_name = "panda_hand"
+
+# Set the goal position of the end effector
+idx_tool = model.getFrameId(frame_name)
+oMtool = data.oMf[idx_tool]
+# Add an offset to the current position
+oMgoal = pin.SE3(oMtool.rotation, oMtool.translation + np.array([0.1, 0.1, 0.1]))
+
+# Rotate the goal by 180 degrees around the x-axis with a rotation matrix
+oMgoal.rotation = oMgoal.rotation @ pin.utils.rotate('x', 3.14)
 
 q_previous = simulate_movement(viz, model, data, q_current, v_desired, new_window)
 
